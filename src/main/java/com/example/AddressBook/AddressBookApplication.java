0d3b556accb474efeff8aa674940a9f8717cf5e5
Neha Agrahari <neha16agrahari@gmail.com>
2025-03-06 18:56:34 +0530

    install

---
+++ src/main/java/com/example/AddressBook/AddressBookApplication.java
@@ -0,0 +1,14 @@
+package com.example.AddressBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		System.out.println("addressbook");
+		SpringApplication.run(AddressBookApplication.class, args);
+	}
+
+}
